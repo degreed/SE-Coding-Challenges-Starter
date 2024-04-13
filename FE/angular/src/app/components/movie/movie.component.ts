de0624dc489ffa22880d389d29847b7b21a58bec
MSI\smyly <smylydon@yahoo.com>
2024-04-13 10:41:47 -0700

    Fixed rxjs in movie. Fixed layout in _movie.scss to (default) flex-direction: row. Fixed button cursor so its a pointer.

--- FE/angular/src/app/components/movie/movie.component.ts
+++ FE/angular/src/app/components/movie/movie.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { tap } from 'rxjs';
+import { Subscription, mergeMap, tap } from 'rxjs';
 import { DataService, MovieComplete } from '../../services/data.service';
 
 @Component({
@@ -9,14 +9,20 @@ import { DataService, MovieComplete } from '../../services/data.service';
 })
 export class MovieComponent implements OnDestroy, OnInit {
   public movie: MovieComplete;
-  public movieId = '';
-  private movieSubscription: any;
+  private movieSubscription: Subscription = new Subscription();
 
   constructor(private activatedRoute: ActivatedRoute, private dataService: DataService) {}
 
   public ngOnInit() {
-    this.activatedRoute.params.pipe(tap(({ id }) => (this.movieId = id)));
-    this.movieSubscription = this.dataService.getMovie(this.movieId).pipe(tap((data) => (this.movie = data)));
+    this.movieSubscription.add(
+      this.activatedRoute.params
+        .pipe(
+          mergeMap(({ id }) => {
+            return this.dataService.getMovie(id as string);
+          })
+        )
+        .subscribe((data: MovieComplete) => (this.movie = data))
+    );
   }
 
   public ngOnDestroy(): void {
