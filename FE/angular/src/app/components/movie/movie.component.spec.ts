2becc7dfd11896326d172cee188d09de7ffadfd4
iamdeeps <deepakbehki@gmail.com>
2024-06-14 18:49:00 +0530

    fix: Issues with movie component spec file

--- FE/angular/src/app/components/movie/movie.component.spec.ts
+++ FE/angular/src/app/components/movie/movie.component.spec.ts
@@ -3,12 +3,14 @@ import { mockProvider, Spectator } from '@ngneat/spectator';
 import { createComponentFactory } from '@ngneat/spectator/jest';
 import { DataService } from '../../services/data.service';
 import { MovieComponent } from './movie.component';
+import { of } from 'rxjs';
+import { mockMovies } from '../../tests/mock-data';
 
 const mockActivatedRoute = mockProvider(ActivatedRoute, {
-  params: jest.fn()
+  params: of(({id: 'tt123'}))
 });
 const mockDataService = mockProvider(DataService, {
-  getMovie: jest.fn()
+  getMovie: jest.fn().mockReturnValue(of({ ...mockMovies[0] }))
 });
 
 describe('MovieComponent', () => {
@@ -26,10 +28,18 @@ describe('MovieComponent', () => {
   beforeEach(() => {
     spectator = createComponent();
     component = spectator.component;
+    component.ngOnInit();
   });
 
   test('should create the component', () => {
-    component.ngOnInit();
     expect(component).toBeTruthy();
   });
+
+  test('should have the expected movie id', () => {
+    expect(component.movieId).toBe('tt123')
+  })
+
+  test('should not have the following movie id', () => {
+    expect(component.movieId).not.toBe('tt124')
+  })
 });
